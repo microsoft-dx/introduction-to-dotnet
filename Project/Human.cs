dc7dbbaa42075f1fb6443e2e51f22b3154b38b44
Xander <petrescu_alexandru96@yahoo.com>
2019-10-16 20:01:06 +0300

    Added Base Project

---
+++ Project/Human.cs
@@ -0,0 +1,38 @@
+﻿namespace Humans
+{
+    class Human
+    {
+        // Those are non-static Members
+        public string Name;
+        private bool isABoy;
+        private int ID;
+
+        // This is a static Member
+        public static int HumanCount = -100;
+
+        // This is a constructor with a parameter
+        public Human(string nameOfHuman)
+        {
+            Name = nameOfHuman;
+            //isABoy = true;
+
+            ID = HumanCount;
+            HumanCount += 1;
+        }
+
+        // This is a method
+        public string FullDescription()
+        {
+            return ID.ToString() + " " + Name;
+        }
+
+        // This is also method
+        public string TellGender()
+        {
+            if (isABoy)
+                return "Male";
+            else
+                return "Female";
+        }
+    }
+}
---
+++ Project/Program.cs
@@ -0,0 +1,62 @@
+﻿using Humans;
+using System;
+
+namespace Classes
+{
+    class Program
+    {
+        static string SecondHello = "I am second";
+
+        // Main Entry Point
+        static void Main(string[] args)
+        {
+            /*
+            Console.Write(CustomHello());
+            Console.Write(CustomHello());
+            */
+            Human John = new Human("John");
+            // John.Name = "John";
+            // John.isABoy= true;
+
+            // Calling the console witht the method of an object
+            Console.WriteLine(John.FullDescription());
+            John.Name += " Something";
+            Console.WriteLine(John.Name);
+            Console.WriteLine(John.TellGender());
+
+            // Creating another Human
+            Human Ana = new Human("Ana");
+
+            // Using the Static Member of a Class
+            Console.WriteLine(Human.HumanCount);
+            Console.WriteLine(Ana.FullDescription());
+
+            _ = new Human("");
+            _ = new Human("");
+            _ = new Human("");
+            _ = new Human("");
+            _ = new Human("");
+            _ = new Human("");
+
+            Console.WriteLine(Human.HumanCount);
+
+            Human.HumanCount += 6;
+
+
+            Console.WriteLine(Human.HumanCount);
+            Console.WriteLine(Ana.FullDescription());
+
+            Console.Read();
+        }
+
+        static string CustomHello()
+        {
+            return "Hello World!";
+        }
+    }
+
+    class NewClass { }
+
+
+
+}
